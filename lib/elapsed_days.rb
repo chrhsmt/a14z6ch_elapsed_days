3bf6c85922c2b2b8f0d415ac2b7d424fffec64b5
Chihiro Hashimoto <a14z6ch@aiit.ac.jp>
2014-10-28 12:38:57 +0000

    簡易的な日付計算実装

--- lib/elapsed_days.rb
+++ lib/elapsed_days.rb
@@ -1,12 +1,14 @@
 require "elapsed_days/version"
 require 'active_support'
 require 'active_support/core_ext'
+require 'time'
 
 module ElapsedDays
   class Base
     class << self 
-      def calc
-        puts "yes"
+      def calc(birthday)
+        elapsed = Time.now - Time.parse(birthday)
+        puts "elapsed is #{elapsed}"
       end
     end
   end
