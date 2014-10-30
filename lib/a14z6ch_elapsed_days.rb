f4a89b9bfdecc2b765bd751875b91aa9574bbb96
Chihiro Hashimoto <a14z6ch@aiit.ac.jp>
2014-10-30 12:00:30 +0000

    change result format

--- lib/a14z6ch_elapsed_days.rb
+++ lib/a14z6ch_elapsed_days.rb
@@ -1,14 +1,30 @@
 require "a14z6ch_elapsed_days/version"
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/number_helper'
 require 'time'
+require 'colorize'
 
+I18n.enforce_available_locales = false
+
+#
+# To calculate elapsed days.
+#
 module A14z6chElapsedDays
   class Base
     class << self 
-      def calc(birthday)
-        elapsed = Time.now - Time.parse(birthday)
-        puts "elapsed is #{elapsed}"
+
+      #
+      # calculating method
+      # @param from date
+      #
+      def calc(date)
+        begin
+          elapsed_seconds = Time.now - Time.parse(date)
+          puts "#{(elapsed_seconds / 60 / 60 / 24).to_i.to_s(:delimited)} days elapsed from the day \"#{date}\""
+        rescue ArgumentError => e
+	  puts "given date is not valid.".colorize(:light_blue)
+	end
       end
     end
   end
