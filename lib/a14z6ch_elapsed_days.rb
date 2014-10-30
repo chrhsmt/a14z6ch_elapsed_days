7cae8e5e2aaa2be1cab39f09a93237f8997b1f75
Chihiro Hashimoto <a14z6ch@aiit.ac.jp>
2014-10-30 12:17:31 +0000

    separate result from module

--- lib/a14z6ch_elapsed_days.rb
+++ lib/a14z6ch_elapsed_days.rb
@@ -1,7 +1,6 @@
 require "a14z6ch_elapsed_days/version"
 require 'active_support'
 require 'active_support/core_ext'
-require 'active_support/number_helper'
 require 'time'
 require 'colorize'
 
@@ -21,7 +20,7 @@ module A14z6chElapsedDays
       def calc(date)
         begin
           elapsed_seconds = Time.now - Time.parse(date)
-          puts "#{(elapsed_seconds / 60 / 60 / 24).to_i.to_s(:delimited)} days elapsed from the day \"#{date}\"".colorize(:light_magenta)
+	  (elapsed_seconds / 60 / 60 / 24).to_i
         rescue ArgumentError => e
 	  puts "given date is not valid.".colorize(:light_blue)
 	end
